482396ad30f13ab9604572511b384a5288ec1a3f
GUCCIZUMI <gutiyosizumi@gmail.com>
2024-05-15 14:45:43 +0900

    ロボットodom位置取得試作

--- src/PF_Visualization.cpp
+++ src/PF_Visualization.cpp
@@ -7,6 +7,10 @@
 #include <geometry_msgs/PoseArray.h>
 #include <random>
 #include <visualization_msgs/MarkerArray.h>
+#include <potbot_lib/Utility.h>
+#include <cmath>
+#include <vector>
+
 
 std::vector<geometry_msgs::Pose> marker_positions;
 
@@ -26,13 +30,29 @@ void Marker_callback(const visualization_msgs::MarkerArray& marker_array)
     }
 }
 
+void RobotPose_Callback(const nav_msgs::Odometry& odom)
+{
+    nav_msgs::Odometry odom_pose ;
+    
+    double x = odom_pose.pose.pose.position.x;
+    double y = odom_pose.pose.pose.position.y;
+    double z = odom_pose.pose.pose.position.z;
+
+    double yaw = potbot_lib::utility::get_Yaw(odom_pose.pose.pose.orientation)
+
+}
+
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "marker_Visualization");
     ros::NodeHandle nh;
 
     // サブスクライバの作成
-    ros::Subscriber sub = n.subscribe("marker", 1000, Marker_callback);
+    ros::Subscriber sub = nh.subscribe("marker", 1000, Marker_callback);
+    ros::Subscriber sub = nh.Subscribe('robot_pose', 1000, RobotPose_Callback);
+
+    ROS_INFO("Position: x=%.2f, y=%.2f, z=%.2f", x, y, z);
+    ROS_INFO("Orientation (RPY): yaw=%.2f", yaw);
 
     // ROSのイベントループを開始
     ros::spin();
